ce8e50a1408f0cb0227fb205ce10b0497c589922
Nadia-wh <64726301+Nadia-wh@users.noreply.github.com>
2024-06-01 10:30:06 +0530

    Update bank.cs

--- bank.cs
+++ bank.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 
 namespace Bank 
@@ -7,21 +7,21 @@ namespace Bank
     {
         // this class bank is dedicated to store and check the bank info of a single person 
         //assuming one person has created an account and bank ID has been allocated
+         //this class  can be used to create an account(later) 
+        //and display the information 
         public string myName ="Anne";
         public double myBalance = 0 ;
-        protected int  bankID = 33;
+
+        // a private access modifier is used to save the password 
+        private string password ="acccount.txt";
 
 
-        
-        //this class  can be used to create an account(later) 
-        //and display the information 
-        
+
         public void showInfo()
         {
-                Console.WriteLine("Your details: ");
+                Console.WriteLine("This is who you are and your balance is : ");
                 Console.WriteLine("Name: "+myName);
                 Console.WriteLine("Balance: " +myBalance);
-                Console.WriteLine("bank id is " + bankID) ;
 
         }
 
