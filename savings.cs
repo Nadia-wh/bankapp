eb538636de8818ed108fe89cf0578057a64562f1
Nadia-wh <64726301+Nadia-wh@users.noreply.github.com>
2024-06-01 10:30:47 +0530

    Update savings.cs

--- savings.cs
+++ savings.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 
 namespace Bank
@@ -9,16 +9,27 @@ namespace Bank
         {
         }
 
-        public override bool deposit(double amount)
+        // Members of Savings can access 'bankID' that was earlier declared protected in base class 
+        public int getID()
+        {
+            return bankID =3;
+        }
+  
+
+
+        public override bool Deposit(double amount)
         {
             this.ammount = amount;
             this.balance = balance + ammount;
+
             Console.WriteLine("You account balance has been deposited.Balance is: " + balance);
             return true;
         }
 
-        public override bool withdraw(double amount)
+        public override bool Withdraw(double amount)
         {
+           Console.WriteLine("The Transaction id initiatied ...... againist ID  " + "  /n" + bankID);
+
             this.ammount = amount;
             this.balance = balance - ammount;
             Console.WriteLine("You account balance has been withdrawed.Balance is: " + balance);
