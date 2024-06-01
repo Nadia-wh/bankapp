06e55b1b16dafe9e365e741144b5c780e995ca90
Nadia-wh <64726301+Nadia-wh@users.noreply.github.com>
2024-06-01 06:57:27 +0530

    Add files via upload

---
+++ account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+
+namespace Bank
+{
+    abstract class Account
+    {
+        public double balance;
+        public double ammount;
+        public abstract bool deposit(double amount);
+
+        public abstract bool withdraw(double amount);
+
+
+
+        
+
+
+    }
+}
---
+++ bank.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Bank 
+{
+    class Bank 
+    {
+        // this class bank is dedicated to store and check the bank info of a single person 
+        //assuming one person has created an account and bank ID has been allocated
+        public string myName ="Anne";
+        public double myBalance = 0 ;
+        protected int  bankID = 33;
+
+
+        
+        //this class  can be used to create an account(later) 
+        //and display the information 
+        
+        public void showInfo()
+        {
+                Console.WriteLine("Your details: ");
+                Console.WriteLine("Name: "+myName);
+                Console.WriteLine("Balance: " +myBalance);
+                Console.WriteLine("bank id is " + bankID) ;
+
+        }
+
+    }    
+}
