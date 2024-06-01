c51f32ae4b1ba217c2dc00dedabaf97e29905766
Nadia-wh <64726301+Nadia-wh@users.noreply.github.com>
2024-06-01 10:32:07 +0530

    Update transaction.cs

--- transaction.cs
+++ transaction.cs
@@ -1,1 +1,41 @@
 
+
+using System;
+
+
+namespace Bank
+{
+    class Savings : Account
+    {
+        public Savings() : base()
+        {
+        }
+
+        // Members of Savings can access 'bankID' that was earlier declared protected in base class 
+        public int getID()
+        {
+            return bankID =3;
+        }
+  
+
+
+        public override bool Deposit(double amount)
+        {
+            this.ammount = amount;
+            this.balance = balance + ammount;
+
+            Console.WriteLine("You account balance has been deposited.Balance is: " + balance);
+            return true;
+        }
+
+        public override bool Withdraw(double amount)
+        {
+           Console.WriteLine("The Transaction id initiatied ...... againist ID  " + "  /n" + bankID);
+
+            this.ammount = amount;
+            this.balance = balance - ammount;
+            Console.WriteLine("You account balance has been withdrawed.Balance is: " + balance);
+            return true;
+        }
+    }
+}
